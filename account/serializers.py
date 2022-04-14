829b7f396a466e3ae5bda5ba0282e0becdddfafc
Baiel1922 <abdyllaevbaiel.00@gmail.com>
2022-04-14 19:27:29 +0600

    added login logout

--- account/serializers.py
+++ account/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 
 from .models import MyUser
@@ -53,3 +54,28 @@ class ActivationSerializer(serializers.Serializer):
         user.is_active = True
         user.activation_code = ''
         user.save()
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        label='password',
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'),
+                                email=email, password=password)
+            if not user:
+                messege = "Unable to log in with provided credentials"
+                raise serializers.ValidationError(messege, code='authorization')
+
+        else:
+            messege = "Must include 'email' and 'password'."
+            raise serializers.ValidationError(messege, code="authorization")
+
+        attrs['user'] = user
+        return attrs
--- account/views.py
+++ account/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
@@ -21,4 +24,15 @@ class ActivationView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.activate()
             return Response('Ваш аккаунт успешно активирован')
+
+class LoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request):
+        user = request.user
+        Token.objects.filter(user=user).delete()
+        return Response("Successfully signed out", status=status.HTTP_200_OK)
 
