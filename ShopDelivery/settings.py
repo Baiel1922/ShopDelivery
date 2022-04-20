e0ac8279e83eca81ddcf892d3f22265bb19d55d4
Baiel1922 <abdyllaevbaiel.00@gmail.com>
2022-04-20 23:29:29 +0600

    added smtp email

--- ShopDelivery/settings.py
+++ ShopDelivery/settings.py
@@ -136,9 +136,9 @@ REST_FRAMEWORK = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "account.MyUser"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'baielabdyllaev.00@gmail.com'
-# EMAIL_HOST_PASSWORD = ''
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'baielabdyllaev.00@gmail.com'
+EMAIL_HOST_PASSWORD = ''
